1fadf048f4f737b02623478ee4bb784fedbd089b
SmartyGrasshopper <parrotabhijeet@gmail.com>
2023-04-29 23:41:27 +0530

    Few changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
 document.querySelector('h2').textContent = 'An image showing an instance of Julia-Sets.';
+//alert(message = 'Welcome');
+
+document.querySelector("html").addEventListener("click", () => {
+    alert("Ouch! Stop poking me!");
+  });
