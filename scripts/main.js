63ca8446de15ae46890b35be35987ea72211314a
SmartyGrasshopper <parrotabhijeet@gmail.com>
2023-04-30 01:02:51 +0530

    Tested and added localStorage usage. Now, site remembers the last image being seen and opens up from there.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
 //document.querySelector('h2').textContent = 'An image showing an instance of Julia-Sets.';
-alert(message = 'Welcome! Click on the Image to cycle between images.');
+alert(message = 'Welcome! Click on the image to cycle between images.');
+//let name = prompt("What is your name?"); // just checking prompt function
 
-let a = 0;
+let a = localStorage.getItem('imageNum');
+if(!a) a = 0;   // if a is not stored earlier
+else a = parseInt(a);
+
 let total = 4;
 let images = [
     "julia_set2_res1000_hot.png",
@@ -10,8 +14,12 @@ let images = [
     "radialSineWaveSurface.png"
 ]
 
+document.getElementById('juliaImage').setAttribute('src', 'images/'+images[a]);  //setting the image to last one being viewed
+
+
 document.getElementById("juliaImage").addEventListener("click", () => {
     a += 1;
     if(a === total) a = 0;
     document.getElementById('juliaImage').setAttribute('src', 'images/'+images[a]);
+    localStorage.setItem('imageNum', a);
 });
