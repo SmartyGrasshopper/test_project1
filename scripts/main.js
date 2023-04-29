7e904e63c3e15c1f23f34d96a9fd033ef1450c2c
SmartyGrasshopper <parrotabhijeet@gmail.com>
2023-04-30 00:23:05 +0530

    Few images added and functionality added to switch between them.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,17 @@
-document.querySelector('h2').textContent = 'An image showing an instance of Julia-Sets.';
-//alert(message = 'Welcome');
+//document.querySelector('h2').textContent = 'An image showing an instance of Julia-Sets.';
+alert(message = 'Welcome! Click on the Image to cycle between images.');
 
-document.querySelector("html").addEventListener("click", () => {
-    alert("Ouch! Stop poking me!");
-  });
+let a = 0;
+let total = 4;
+let images = [
+    "julia_set2_res1000_hot.png",
+    "julia_set7_res1000_viridis.png",
+    "mandelbrot_res5000_magma.png",
+    "radialSineWaveSurface.png"
+]
+
+document.getElementById("juliaImage").addEventListener("click", () => {
+    a += 1;
+    if(a === total) a = 0;
+    document.getElementById('juliaImage').setAttribute('src', 'images/'+images[a]);
+});
