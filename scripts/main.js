f1f64ddbe7947cb43578c60f6571dc08d0c2c471
SmartyGrasshopper <parrotabhijeet@gmail.com>
2023-05-01 19:57:05 +0530

    Adjustments to add figure caption to the image.

--- scripts/main.js
+++ scripts/main.js
@@ -7,21 +7,23 @@ else a = parseInt(a);
 
 let total = 4;
 let images = [
-    "julia_set2_res1000_hot.png",
-    "julia_set7_res1000_viridis.png",
-    "mandelbrot_res5000_magma.png",
-    "radialSineWaveSurface.png"
+    ["julia_set2_res1000_hot.png", "An instance of julia-set."],
+    ["julia_set7_res1000_viridis.png", "An instance of julia-set."],
+    ["mandelbrot_res5000_magma.png", "Mandelbrot-set"],
+    ["radialSineWaveSurface.png", "Sine waves spreading radially out from the origin."]
 ]
 
-document.getElementById('juliaImage').setAttribute('src', 'images/' + images[a]);  //setting the image to last one being viewed
-document.getElementById('imageLink').setAttribute('href', 'images/' + images[a]);
+document.getElementById('juliaImage').setAttribute('src', 'images/' + images[a][0]);  //setting the image to last one being viewed
+document.getElementById('imageLink').setAttribute('href', 'images/' + images[a][0]);
+document.getElementById('juliaImageCaption').textContent = images[a][1];
 
 
 document.getElementById("juliaImage").addEventListener("click", () => {
     a += 1;
     if (a === total) a = 0;
-    document.getElementById('juliaImage').setAttribute('src', 'images/' + images[a]);
-    document.getElementById('imageLink').setAttribute('href', 'images/' + images[a]);
+    document.getElementById('juliaImage').setAttribute('src', 'images/' + images[a][0]);
+    document.getElementById('imageLink').setAttribute('href', 'images/' + images[a][0]);
+    document.getElementById('juliaImageCaption').textContent = images[a][1];
     localStorage.setItem('imageNum', a);
 });
 
